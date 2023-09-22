3657ba7d0afc2c4707b7e7aec82d8923749b49e5
UserPierreG <pierrelouisguichard@gmail.com>
2023-09-22 15:06:58 +0100

    v0.2 Working on the Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from "react";
-import "./main.css";
+import Navbar from "./navbar";
 
 function App() {
-  return <div style={{ color: "white" }}>Hello</div>;
+  return (
+    <div style={container}>
+      <Navbar />
+    </div>
+  );
 }
+
+const container = {
+  backgroundColor: "black",
+  top: 0,
+  left: 0,
+  position: "absolute",
+  height: "100vh",
+  width: "100vw",
+  color: "white",
+};
 
 export default App;
